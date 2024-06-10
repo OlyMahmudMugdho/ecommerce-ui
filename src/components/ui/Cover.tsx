ed9432c0aa11a735a7ead202b7f9549ec307e246
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-10 13:06:40 +0600

    width full

--- src/components/ui/Cover.tsx
+++ src/components/ui/Cover.tsx
@@ -7,7 +7,7 @@ type Props = {}
 
 function Cover({ }: Props) {
     return (
-        <Box sx={{ p: 3, width: { sm: "200px", lg: "1200px" } }} alignContent={""} display={"flex"} justifyContent="center">
+        <Box className="w-full" sx={{ p: 3, width: { sm: "200px", lg: "1200px" } }} alignContent={""} display={"flex"} justifyContent="center">
             <Image alt="banner" src={banner} />
         </Box>
     )
