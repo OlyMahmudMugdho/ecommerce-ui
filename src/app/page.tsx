3f1eff8721cf9359b8a2a530f1b481423d3b055d
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-10 13:03:15 +0600

    css tweak

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+import Cover from "@/components/ui/Cover";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between py-5">
+      <Cover />
+    </main>
+  );
+}
