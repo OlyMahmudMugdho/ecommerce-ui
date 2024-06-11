46862978935aa6958b96bbd53eed14459252a751
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-11 12:12:08 +0600

    added cards

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -2,21 +2,24 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import ProductCard from './ProductCard'
 
 type Props = {
   header: string,
-  //cards: CardInfo[]
+  cards: CardInfo[]
 }
 
 export type CardInfo = {
   name: string,
   category: string,
-  image: string
+  image: string,
+    description: string,
+    link: string
 }
 
 export default function Section(props : Props) {
   return (
-    <Box className='flex justify-between items-center' component="section" sx={{ p: 2}}>
+    <Box className='flex flex-col justify-between items-center' component="section" >
       <Box
           className='flex w-full items-center justify-between'
           display="flex"
@@ -28,6 +31,25 @@ export default function Section(props : Props) {
 
           <Button variant="outlined">SEE MORE</Button>
       </Box>
+
+        <Box
+            className='flex w-full items-center justify-between'
+            display="flex"
+            alignItems="center"
+        >
+            {  props.cards.map(
+                item => <ProductCard
+                    key={item.link}
+                    name={item.name}
+                    category={item.category}
+                    image={item.image}
+                    description={item.description}
+                    link={item.link}
+                />
+            )}
+        </Box>
+
+
     </Box>
   );
 }
