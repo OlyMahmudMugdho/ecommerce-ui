7e0201e4ef4cf6dcc7db3ab4363d8942522c1b3e
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-11 10:55:12 +0600

    added content component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 
 import Cover from "@/components/ui/Cover";
+import Content from "@/components/ui/Content";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-5">
       <Cover />
+        <Content />
     </main>
   );
 }
