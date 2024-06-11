ff81db7c5aa7f969070ceb6bf9f2e4d168216e6f
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-11 14:26:28 +0600

    made the carousel

--- src/components/ui/ProductCarouselItem.tsx
+++ src/components/ui/ProductCarouselItem.tsx
@@ -3,41 +3,54 @@ import AspectRatio from '@mui/joy/AspectRatio';
 import Box from '@mui/joy/Box';
 import Typography from '@mui/joy/Typography';
 import Card from '@mui/joy/Card';
-import {CardInfo} from "@/components/ui/Section";
+import {CardInfo, CardProps} from "@/components/ui/Section";
+import Image from "next/image";
+import Button from "@mui/material/Button";
 
 
 
-export default function CarouselRatio(props : CardInfo[]) {
+export default function CarouselRatio(props : CardProps) {
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                gap: 1,
-                py: 1,
-                overflow: 'auto',
-                width: 343,
-                scrollSnapType: 'x mandatory',
-                '& > *': {
-                    scrollSnapAlign: 'center',
-                },
-                '::-webkit-scrollbar': { display: 'none' },
-            }}
-        >
-            {props.map((item) => (
-                <Card orientation="horizontal" size="sm" key={item.name} variant="outlined">
-                    <AspectRatio ratio="1" sx={{ minWidth: 60 }}>
-                        <img
-                            srcSet={`${item.link}?h=120&fit=crop&auto=format&dpr=2 2x`}
-                            src={`${item.link}?h=120&fit=crop&auto=format`}
-                            alt={item.name}
-                        />
-                    </AspectRatio>
-                    <Box sx={{ whiteSpace: 'nowrap', mx: 1 }}>
-                        <Typography level="title-md">{item.name}</Typography>
-                        <Typography level="body-sm">{item.description}</Typography>
-                    </Box>
-                </Card>
-            ))}
+        <Box className={"w-full flex flex-col"}>
+            <Box className='flex w-full items-center justify-between'
+                 display="flex"
+                 alignItems="center"
+            >
+                <Typography className={'text-xl'} component="h4">
+                    {props.header}
+                </Typography>
+                <Button variant="outlined">SEE MORE</Button>
+            </Box>
+            <Box
+                className={"w-full"}
+                sx={{
+                    display: 'flex',
+                    gap: 1,
+                    py: 1,
+                    overflow: 'auto',
+                    width: 343,
+                    scrollSnapType: 'x mandatory',
+                    '& > *': {
+                        scrollSnapAlign: 'center',
+                    },
+                    '::-webkit-scrollbar': { display: 'none' },
+                }}
+            >
+                {props.cards.map((item) => (
+                    <Card className={"w-full"} orientation="horizontal" size="sm" key={item.name} variant="outlined">
+                        <AspectRatio ratio="1" sx={{ minWidth: 60 }}>
+                            <img
+                                src={`${item.image}?h=120&fit=crop&auto=format`}
+                                alt={item.name}
+                            />
+                        </AspectRatio>
+                        <Box sx={{ whiteSpace: 'nowrap', mx: 1 }}>
+                            <Typography level="title-md">{item.name}</Typography>
+                            <Typography level="body-sm">{item.description}</Typography>
+                        </Box>
+                    </Card>
+                ))}
+            </Box>
         </Box>
     );
 }
