7301d3de95a44c28f336338ee5cea08709f70b61
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-11 11:41:56 +0600

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import PrimarySearchAppBar from "@/components/navigation/Navbar";
-
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-
+export const metadata : Metadata = {
+    title : "E-Commerce",
+    description : "Online Shopping"
+}
 
 export default function RootLayout({
   children,
@@ -16,8 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <body className={inter.className}>
-        <PrimarySearchAppBar suppressHydrationWarning={true}></PrimarySearchAppBar>
+      <body className={inter.className +  "min-h-screen"}>
+        <PrimarySearchAppBar suppressHydrationWarning></PrimarySearchAppBar>
         {children}
         
       </body>
