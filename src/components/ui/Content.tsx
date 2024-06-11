b69581f6676a629c43d2315d2df0b5b4a02f9fc4
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-11 12:13:24 +0600

    removed * import

--- src/components/ui/Content.tsx
+++ src/components/ui/Content.tsx
@@ -1,11 +1,20 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
-import Section from "./Section"
+import Section, {CardInfo} from "./Section"
+
+const newProducts : CardInfo[] = [
+    {
+        name:  "Black Shirt",
+        category: "Fashion",
+        image: "https://google.com",
+        description : "some shirts",
+        link : "http://localhost:3000"
+    }
+]
 
 export default function Content() {
   return (
     <Box className='w-full px-5' component="section">
-      <Section header={"New Products"} />
+      <Section header={"New Products"} cards={newProducts} />
     </Box>
   );
 }
