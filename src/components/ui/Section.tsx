d3cac64bd0ebe72f1842c3cde26e161037482e86
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-11 11:43:00 +0600

    exported CardInfo type

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -1,33 +1,32 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 type Props = {
   header: string,
-  cards: CardInfo[]
+  //cards: CardInfo[]
 }
 
-type CardInfo = {
+export type CardInfo = {
   name: string,
   category: string,
   image: string
 }
 
-export default function Section() {
+export default function Section(props : Props) {
   return (
-    <Box className='flex justify-between items-center' component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+    <Box className='flex justify-between items-center' component="section" sx={{ p: 2}}>
       <Box
           className='flex w-full items-center justify-between'
           display="flex"
           alignItems="center"
       >
-          <Typography variant="h4" component="h4">
-              h1. Heading
+          <Typography className={'text-2xl'} variant="h4" component="h4">
+              {props.header}
           </Typography>
 
-          <Typography variant="h4" component="h4">
-              h1. Heading
-          </Typography>
+          <Button variant="outlined">SEE MORE</Button>
       </Box>
     </Box>
   );
