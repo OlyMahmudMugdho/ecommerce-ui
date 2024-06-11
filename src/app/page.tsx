bdfd6c23f6316e3d7f05a3672e63f65cbbd1be80
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-11 11:43:34 +0600

    removed min-h-screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Cover from "@/components/ui/Cover";
 import Content from "@/components/ui/Content";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-5">
+    <main className="flex flex-col items-center justify-between py-5">
       <Cover />
         <Content />
     </main>
