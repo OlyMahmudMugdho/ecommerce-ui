f39292f7b89a23f9446dea9fd4affb640b337492
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-15 18:36:15 +0600

    changed footer color

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -7,97 +7,99 @@ const Footer: React.FC = () => {
         <Box
             component="footer"
             sx={{
-                py: 3,
+                py: 4,
                 px: 2,
                 mt: 'auto',
-                backgroundColor: '#f8f8f8', // Static background color
+                backgroundColor: '#282c34',
+                color: '#fff',
+                boxShadow: '0px -2px 5px rgba(0,0,0,0.2)',
             }}
         >
             <Container maxWidth="lg">
                 <Grid container spacing={4}>
                     <Grid item xs={12} sm={6} md={3}>
-                        <Typography variant="h6" color="textPrimary" gutterBottom>
+                        <Typography variant="h6" color="inherit" gutterBottom>
                             Company
                         </Typography>
-                        <ul>
+                        <ul style={{ listStyleType: 'none', padding: 0 }}>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     About Us
                                 </Link>
                             </li>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Careers
                                 </Link>
                             </li>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Press
                                 </Link>
                             </li>
                         </ul>
                     </Grid>
                     <Grid item xs={12} sm={6} md={3}>
-                        <Typography variant="h6" color="textPrimary" gutterBottom>
+                        <Typography variant="h6" color="inherit" gutterBottom>
                             Support
                         </Typography>
-                        <ul>
+                        <ul style={{ listStyleType: 'none', padding: 0 }}>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Help Center
                                 </Link>
                             </li>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Contact Us
                                 </Link>
                             </li>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     FAQs
                                 </Link>
                             </li>
                         </ul>
                     </Grid>
                     <Grid item xs={12} sm={6} md={3}>
-                        <Typography variant="h6" color="textPrimary" gutterBottom>
+                        <Typography variant="h6" color="inherit" gutterBottom>
                             Legal
                         </Typography>
-                        <ul>
+                        <ul style={{ listStyleType: 'none', padding: 0 }}>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Terms of Service
                                 </Link>
                             </li>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Privacy Policy
                                 </Link>
                             </li>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Cookie Policy
                                 </Link>
                             </li>
                         </ul>
                     </Grid>
                     <Grid item xs={12} sm={6} md={3}>
-                        <Typography variant="h6" color="textPrimary" gutterBottom>
+                        <Typography variant="h6" color="inherit" gutterBottom>
                             Follow Us
                         </Typography>
-                        <ul>
+                        <ul style={{ listStyleType: 'none', padding: 0 }}>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Facebook
                                 </Link>
                             </li>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Twitter
                                 </Link>
                             </li>
                             <li>
-                                <Link href="#" variant="subtitle1" color="textSecondary">
+                                <Link href="#" variant="subtitle1" color="inherit">
                                     Instagram
                                 </Link>
                             </li>
@@ -105,7 +107,7 @@ const Footer: React.FC = () => {
                     </Grid>
                 </Grid>
                 <Box mt={5}>
-                    <Typography variant="body2" color="textSecondary" align="center">
+                    <Typography variant="body2" color="inherit" align="center">
                         {'© '}
                         <Link color="inherit" href="/">
                             Your E-commerce Platform
